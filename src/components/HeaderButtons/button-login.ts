0e2a0865f97041f7c004cf45418ca4a68467bde4
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-02-07 00:50:35 +0400

    refactor: rename href in button-login.ts

--- src/components/HeaderButtons/button-login.ts
+++ src/components/HeaderButtons/button-login.ts
@@ -1,10 +1,10 @@
-import { ButtonTemlate } from "./button-template";
+import { ButtonTemlate } from './button-template';
 
 export const ButtonLoginComponent = {
   render: () => {
     return `          
-        ${ButtonTemlate('#/sign-in', 'Вход')}
-        <a href="#/sign-up" class="btn btn-outline-secondary sign-up-btn">Регистрация</a>
+        ${ButtonTemlate('#/login', 'Вход')}
+        <a href="#/registration" class="btn btn-outline-secondary sign-up-btn">Регистрация</a>
       `;
   },
 };
