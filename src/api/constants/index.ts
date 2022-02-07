15fcd120166d3cefa5cd4f0644450a1409893487
Nick1091 <nick10.91@mail.ru>
2022-02-07 20:12:46 +0300

    rslang-46 feat: add folder constants

---
+++ src/api/constants/index.ts
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://learnwords-team22.herokuapp.com/';
+// const baseUrl = 'https://react-learnwords-example.herokuapp.com/';
+// const baseUrl = 'http://localhost:5500/';
+export const wordsUrl = `${baseUrl}words`;
+export const usersUrl = `${baseUrl}users`;
