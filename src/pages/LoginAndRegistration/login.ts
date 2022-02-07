ebcaaeb499608573c288145ae8a54bee822463cd
Nick1091 <nick10.91@mail.ru>
2022-02-07 23:18:17 +0300

    rslang-06 feat: add render function to login and to registration

--- src/pages/LoginAndRegistration/login.ts
+++ src/pages/LoginAndRegistration/login.ts
@@ -1,7 +1,22 @@
+// import { listenSingInPage } from '.';
+import './style.scss';
+
 export const LoginComponent = {
-  listen: () => {},
-  render: () => {
-    return `
-    `;
-  },
+  // listen: () => listenSingInPage(),
+  render: () =>
+    `<div class = "page-singIn-user">
+      <form action="">
+        <h2>Вход</h2>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"></label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ваш email*" autofocus required>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label"></label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="ваш пароль*" required>
+        </div>
+        <p class=form-message><p>
+        <button type="submit" class="btn btn-primary">Подтвердить</button>
+      </form>
+    </div>`,
 };
