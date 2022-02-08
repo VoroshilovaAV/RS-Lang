4008d603c7e8ddfec72eb39888b127aa43eb7534
Nick1091 <nick10.91@mail.ru>
2022-02-08 18:52:21 +0300

    rslang-06 feat: add preloader and add logout

--- src/pages/LoginAndRegistration/utils/authorization/index.ts
+++ src/pages/LoginAndRegistration/utils/authorization/index.ts
@@ -1,21 +1,33 @@
 import { loginUser, createUser } from 'api';
+import { preloadLoad, removePreload } from 'components';
 import { userCreated, authorizedUser } from 'state';
 import { successLoginMessage } from '../';
 import { IUser } from './interfaces';
 
 const loginIn = async (user: IUser) => {
-  const UserData = await loginUser(user);
-  if (UserData) {
-    Object.assign(authorizedUser, UserData);
-    localStorage.setItem('authorizedUser', JSON.stringify(authorizedUser));
-    successLoginMessage();
-  } else {
-    const messageError = document.querySelector('.form-message');
-    if (!(messageError instanceof HTMLElement)) {
-      throw new Error('Error');
-    }
-    messageError.textContent = 'пользователя с такими адресом не существует';
+  const parentElement = document.querySelector('form')?.parentElement;
+  if (!(parentElement instanceof HTMLElement)) {
+    throw new Error('Error');
   }
+  if (!parentElement.classList.contains('loaded_hiding')) {
+    preloadLoad(parentElement);
+  }
+  console.log(parentElement);
+  const UserData = await loginUser(user);
+  removePreload(parentElement);
+  setTimeout(() => {
+    if (UserData) {
+      Object.assign(authorizedUser, UserData);
+      localStorage.setItem('authorizedUser', JSON.stringify(authorizedUser));
+      successLoginMessage();
+    } else {
+      const messageError = document.querySelector('.form-message');
+      if (!(messageError instanceof HTMLElement)) {
+        throw new Error('Error');
+      }
+      messageError.textContent = 'пользователя с такими адресом не существует';
+    }
+  }, 300);
 };
 
 export const getAuthorization = async (user: IUser) => {
@@ -23,16 +35,24 @@ export const getAuthorization = async (user: IUser) => {
 };
 
 export const getNewUser = async (user: IUser) => {
+  const parentElement = document.querySelector('form')?.parentElement;
+  if (!(parentElement instanceof HTMLElement)) {
+    throw new Error('Error');
+  }
+  preloadLoad(parentElement);
   const createdUser = await createUser(user);
   if (createdUser) {
     Object.assign(userCreated, createdUser);
     localStorage.setItem('userCreated', JSON.stringify(userCreated));
     loginIn(user);
   } else {
-    const messageError = document.querySelector('.form-message');
-    if (!(messageError instanceof HTMLElement)) {
-      throw new Error('Error');
-    }
-    messageError.textContent = 'такой адрес уже существует, перейдите на страницу входа';
+    removePreload(parentElement);
+    setTimeout(() => {
+      const messageError = document.querySelector('.form-message');
+      if (!(messageError instanceof HTMLElement)) {
+        throw new Error('Error');
+      }
+      messageError.textContent = 'такой адрес уже существует, перейдите на страницу входа';
+    }, 300);
   }
 };
