fae4ab510d720c163c25b072aa1de51bfb44f09e
Igor <igoralyanoy@gmail.com>
2022-02-14 18:27:18 +0200

    rslang-39 refactor: create consts for compare func

--- src/pages/Games/Sprint/utils/changeAndCompareText/index.ts
+++ src/pages/Games/Sprint/utils/changeAndCompareText/index.ts
@@ -1,5 +1,6 @@
 import { randomNum } from 'pages/Games/utils/randomNum';
 import { gameState } from 'state';
+import { TRANSLATES_COMPARE_AMOUNT } from '../consts';
 
 const changeHTMLWordsText = (word: string, translate: string) => {
   const wordText = document.querySelector('.sprint__answer-eng');
@@ -9,16 +10,15 @@ const changeHTMLWordsText = (word: string, translate: string) => {
 };
 
 export const changeAndCompareText = () => {
-  if (
-    (gameState.translates.length - gameState.words.length) % 3 === 0 &&
-    gameState.translates.length - gameState.words.length !== 0
-  ) {
-    gameState.translates.splice(gameState.translates.length - 3, 3);
+  const translatesLengthRemainder = gameState.translates.length - gameState.words.length;
+  const restOfTranslates = gameState.translates.length - TRANSLATES_COMPARE_AMOUNT;
+  if (translatesLengthRemainder % TRANSLATES_COMPARE_AMOUNT === 0 && translatesLengthRemainder !== 0) {
+    gameState.translates.splice(restOfTranslates, TRANSLATES_COMPARE_AMOUNT);
   }
   const wordObj = gameState.words.pop();
-  const translate = gameState.translates[randomNum(gameState.translates.length - 3, gameState.translates.length)];
+  const translate = gameState.translates[randomNum(restOfTranslates, gameState.translates.length)];
   if (wordObj && translate) {
     changeHTMLWordsText(wordObj.word, translate);
-    return wordObj.wordTranslate === translate;
+    return { wordObj, isRightTranslate: wordObj.wordTranslate === translate };
   }
 };
