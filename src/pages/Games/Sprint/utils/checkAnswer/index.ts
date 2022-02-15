06dba681ffbed6915acbaa0b3690ef0b43e68651
Igor <igoralyanoy@gmail.com>
2022-02-16 01:00:17 +0200

    rslang-39 feat: add redirect to result page when words array is empty

--- src/pages/Games/Sprint/utils/checkAnswer/index.ts
+++ src/pages/Games/Sprint/utils/checkAnswer/index.ts
@@ -1,24 +1,29 @@
 import { IWord } from 'api/interfaces';
-import { gameState } from 'state';
+import { sprintState } from 'state';
 import { changeAndCompareText } from '../changeAndCompareText';
 import { changeGameScore } from '../changeGameScore';
 import { iconAnimation } from '../iconAnimation';
+import { handleArrowsForSprint } from '../handleBtns';
 
 const separateWordsByAnswer = (isRight: boolean, word: IWord | undefined) => {
   if (word) {
     if (isRight) {
-      gameState.rightAnswers = [...gameState.rightAnswers, word];
+      sprintState.rightAnswers = [...sprintState.rightAnswers, word];
     } else {
-      gameState.wrongAnswers = [...gameState.wrongAnswers, word];
+      sprintState.wrongAnswers = [...sprintState.wrongAnswers, word];
     }
   }
 };
 
 export const checkAnswer = (answerOfBtn: boolean) => {
   const word = changeAndCompareText();
-  const isRight = answerOfBtn === gameState.isRightTranslate;
-  gameState.isRightTranslate = word?.isRightTranslate;
+  const isRight = answerOfBtn === sprintState.isRightTranslate;
+  sprintState.isRightTranslate = word?.isRightTranslate;
   separateWordsByAnswer(isRight, word?.wordObj);
   changeGameScore(isRight);
   iconAnimation(isRight);
+  if (!sprintState.words.length) {
+    document.removeEventListener('keyup', handleArrowsForSprint);
+    location.hash = '/sprint-result';
+  }
 };
--- src/pages/Games/Sprint/utils/sprintListener/index.ts
+++ src/pages/Games/Sprint/utils/sprintListener/index.ts
@@ -1,13 +1,16 @@
-import { gameState } from 'state';
+import { sprintState } from 'state';
 import { updateSprintState } from './../updateSprintState';
 import { startTimer } from '../sprintTimer';
 import { changeAndCompareText } from '../changeAndCompareText';
 import { handleArrowsForSprint, handleBtnsForSprint } from '../handleBtns';
 
 export const sprintListener = () => {
+  if (!sprintState.pageWords.length) {
+    location.hash = '/games';
+  }
   updateSprintState();
   const answerBtns = document.querySelector('.sprint__answer-btns');
-  gameState.isRightTranslate = changeAndCompareText()?.isRightTranslate;
+  sprintState.isRightTranslate = changeAndCompareText()?.isRightTranslate;
 
   answerBtns?.addEventListener('click', handleBtnsForSprint);
   document.addEventListener('keyup', handleArrowsForSprint);
