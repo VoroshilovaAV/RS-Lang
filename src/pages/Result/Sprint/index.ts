a543696f511ca9fda8d27a22f3b5bd06ee34985c
Igor <igoralyanoy@gmail.com>
2022-02-16 01:06:01 +0200

    rslang-39 feat: add results for sprint

--- src/pages/Result/Sprint/index.ts
+++ src/pages/Result/Sprint/index.ts
@@ -1,8 +1,26 @@
+import { IWord } from 'api/interfaces';
+import { sprintState } from 'state';
 import { GameResultPage } from 'components/GameResultTemplate';
+import { createResultWord } from 'components/GameResultTemplate/utils';
+import { setDefaultOptionsToSprintState } from 'state/utils';
+import { soundResultWordListener } from './utils';
 
 export const ResultSprintComponent = {
-  listen: () => {},
+  listen: () => {
+    if (!sprintState.rightAnswers.length && !sprintState.wrongAnswers.length) {
+      location.hash = '/games';
+    }
+    const answers = document.querySelector('.answers');
+    answers?.addEventListener('click', soundResultWordListener);
+    setDefaultOptionsToSprintState();
+  },
   render: () => {
-    return `${GameResultPage('sprint')}`;
+    const correctHTMLWords = sprintState.rightAnswers.map((wordObj: IWord) =>
+      createResultWord('correct-word', wordObj.word, wordObj.wordTranslate, wordObj.audio)
+    );
+    const incorrectHTMLWords = sprintState.wrongAnswers.map((wordObj: IWord) =>
+      createResultWord('incorrect-word', wordObj.word, wordObj.wordTranslate, wordObj.audio)
+    );
+    return `${GameResultPage('sprint', correctHTMLWords.join(''), incorrectHTMLWords.join(''))}`;
   },
 };
