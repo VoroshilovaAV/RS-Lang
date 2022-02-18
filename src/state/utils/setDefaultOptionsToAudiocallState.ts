b3a788cfaed23d6ba9fcd378148b5189dcc8694b
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-02-19 00:34:06 +0400

    fix: set default options for additional keys

--- src/state/utils/setDefaultOptionsToAudiocallState.ts
+++ src/state/utils/setDefaultOptionsToAudiocallState.ts
@@ -1,9 +1,11 @@
 import { audiocallState } from 'state';
 
 export const setDefaultOptionsToAudiocallState = () => {
+  audiocallState.counter = 0;
   audiocallState.score = 0;
   audiocallState.series = 0;
   audiocallState.longestSeries = 0;
   audiocallState.rightAnswers = [];
   audiocallState.wrongAnswers = [];
+  audiocallState.responseNumber = [];
 };
